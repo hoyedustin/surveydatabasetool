1adb72a92b58663cc4b20884f663788b1326c591
hoyedustin <110954384+hoyedustin@users.noreply.github.com>
2025-12-09 12:47:22 -0800

    Update models.py

--- models.py
+++ models.py
@@ -8,4 +8,4 @@ class SurveyResponse(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
-    response = Column(String(500), nullable=False)
+    feedback = Column(String(500), nullable=False)
